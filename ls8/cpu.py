0f3bc2220a062c1a71141271407a6df637f600ea
Steele Helbling <steelehelbling@gmail.com>
2020-09-14 17:55:46 -0400

    ver-1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.PC = 0
+        self.reg = [0] * 8 
+        self.ram = [0] * 256 
+
+
+    def ram_read(self, value):
+        return self.ram[value]
+
+
+    def ram_write(self,value,new):
+        self.ram[value] = new
 
     def load(self):
         """Load a program into memory."""
@@ -30,7 +40,6 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -47,12 +56,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -62,4 +71,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+        running = True
+
+        while running == True:
+            instruction = self.ram_read(self.PC)
+            first_opperator = self.ram_read(self.PC + 1)
+            second_opperator = self.ram_read(self.PC + 2)
+
+            if instruction == HLT:
+                running = False
+                self.PC = self.PC + 1
+
+            elif instruction == LDI:
+                self.reg[first_opperator] = second_opperator
+                self.PC = self.PC + 3 
+
+            elif instruction == PRN:
+                print(self.reg[first_opperator])
+                self.PC = self.PC + 2
+
+            else:
+                print(f" input {bin(instruction)}")
+                running = False
