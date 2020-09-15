cab212814fa1a05250f4c4aca531965a3d30037f
Steele Helbling <steelehelbling@gmail.com>
2020-09-15 18:24:57 -0400

    ver-2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,10 @@
 """CPU functionality."""
 
 import sys
-
+PRN = 0b01000111
+HLT = 0b00000001
+LDI = 0b10000010
+MUL = 0b10100010
 class CPU:
     """Main CPU class."""
 
@@ -10,7 +13,7 @@ class CPU:
         self.PC = 0
         self.reg = [0] * 8 
         self.ram = [0] * 256 
-
+        self.running = True
 
     def ram_read(self, value):
         return self.ram[value]
@@ -26,19 +29,42 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        if len(sys.argv) != 2:
+            print("usage: comp.py progname")
+            sys.exit(1)
+
+        try:
+            with open(sys.argv[1]) as files:
+                for i in files:
+                    i = i.split("#")
+                    i = i[0].strip()
+
+                    if i == "":
+                        continue
+                    else:
+                        self.ram[address] = int(i, 2)
+                        address += 1
+
+        except FileNotFoundError:
+            print(f"File not found {sys.argv[1]}")
+            sys.exit(2)
+
+        if address == 0:
+            print("Program was empty!")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -46,6 +72,7 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -81,7 +108,7 @@ class CPU:
             instruction = self.ram_read(self.PC)
             first_opperator = self.ram_read(self.PC + 1)
             second_opperator = self.ram_read(self.PC + 2)
-
+            print(self.ram_read(self.PC))
             if instruction == HLT:
                 running = False
                 self.PC = self.PC + 1
@@ -90,10 +117,21 @@ class CPU:
                 self.reg[first_opperator] = second_opperator
                 self.PC = self.PC + 3 
 
-            elif instruction == PRN:
-                print(self.reg[first_opperator])
+            elif instruction == PRN:#prints first opperator 8
+                # print(self.reg[first_opperator])
                 self.PC = self.PC + 2
 
+            elif instruction == MUL:# 8 * 9
+                self.reg[first_opperator] = self.reg[first_opperator] * self.reg[second_opperator]
+                self.PC = self.PC + 3
             else:
-                print(f" input {bin(instruction)}")
+                print(f" input {instruction}")
                 running = False
+
+# * `LDI`: load "immediate", store a value in a register, or "set this register to
+#   this value".
+
+# * `PRN`: a pseudo-instruction that prints the numeric value stored in a
+#   register.
+
+# * `HLT`: halt the CPU and exit the emulator.
